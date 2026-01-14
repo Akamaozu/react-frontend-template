9a0304375b8a89366e98c19188e9f01e0e9f0d09
uzo olisemeka <uzo@designbymobius.ca>
2026-01-14 16:22:12 -0500

    add lifecycle logging to production server

--- server/index.js
+++ server/index.js
@@ -6,13 +6,18 @@ utils.env.load()
 const server = utils.server.create()
 const port = process.env.PORT ?? 3000
 
-middleware.build_app.utils.async_check_and_maybe_build_app()
-
 server.use( middleware.build_app )
-server.use( middleware.serve_from_dir([ './public', './build' ]) )
+
+const dirs_to_serve = [ './public', './build' ]
+server.use( middleware.serve_from_dir( dirs_to_serve ) )
+console.log( `action=setup-middleware behavior=serve-from-fs dirs="${ dirs_to_serve.join('", "') }"` )
 
 server.get( '*', middleware.serve_prebuild, middleware.serve_app )
+console.log( 'action=setup-route-handler path=ALL method=GET behavior=serve-app-build-or-prebuild' )
 
 server.listen( port, () => {
   console.log(`action=server-listen port=${ port }`)
+
+  middleware.build_app.utils.async_check_and_maybe_build_app()
+  console.log( 'action=ensure-react-app-build-exists' )
 })
