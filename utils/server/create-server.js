f120f40c94211e10a1ad099e1ca6b6e5abcf5f25
uzo olisemeka <uzo@designbymobius.ca>
2026-01-22 14:24:44 -0500

    add server framework to creation log entry

--- utils/server/create-server.js
+++ utils/server/create-server.js
@@ -4,7 +4,7 @@ module.exports = create_server
 
 function create_server() {
   const server = express()
-  console.log('action=create-server')
+  console.log('action=create-server framework=express')
 
   return server
 }
