2fcaaa4bd7cc868746f189877a89e6cd2ab7ce43
uzo olisemeka <uzo@designbymobius.ca>
2026-01-22 14:45:08 -0500

    fix path to app build in utils

--- utils/app/is-built.js
+++ utils/app/is-built.js
@@ -1,6 +1,8 @@
+const path = require('path')
+const utils = require('../')
+
 module.exports = async_is_built
 
 async function async_is_built() {
-  const utils = require('../')
-  return utils.fs.async_path_exists(__dirname + '/../../../build/index.html')
+  return utils.fs.async_path_exists( path.join(__dirname, '../../build/index.html') )
 }
